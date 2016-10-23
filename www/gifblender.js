adcd94669f5db41526cbf7ad482067324773bc9b
Varsity Software <github@varsitysoftware.com>
2016-10-23 15:19:13 -0400

    Update gifblender.js

--- www/gifblender.js
+++ www/gifblender.js
@@ -10,14 +10,17 @@ GifBlender.prototype.blend = function (success, fail, options)
     }
 
     var params = {
-        fileURL: options.fileURL ? options.fileURL : null,
+        url: options.url ? options.url : null,
+        filter: options.filter ? options.filter : null,
         cssPath: options.cssPath ? options.cssPath : null,
-        overlay: options.overlay ? options.overlay : null,
-        htmlString: options.htmlString ? options.htmlString : null,
+        overlayBase64: options.overlayBase64 ? options.overlayBase64 : null,
+        loadDelay: options.loadDelay ? options.loadDelay : null,
+        quality: options.quality ? options.quality : 0
     };
 
     return cordova.exec(success, fail, "GifBlender", "blend", [params]);
 
 };
+
 
 window.gifBlender = new GifBlender();
