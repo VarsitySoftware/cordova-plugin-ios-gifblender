9e6a38ae80caa5e779058327d6ac92d5627c2e92
Varsity Software <github@varsitysoftware.com>
2016-10-23 08:00:26 -0400

    Update GIFBlender.h

--- src/ios/GIFBlender.h
+++ src/ios/GIFBlender.h
@@ -11,6 +11,7 @@
 @interface GIFBlender : CDVPlugin < UINavigationControllerDelegate, UIScrollViewDelegate>
 
 @property (copy)   NSString* callbackId;
+@property (nonatomic, retain) IBOutlet UIWebView *webView;
 
 - (void)blend:(CDVInvokedUrlCommand *)command;
 
