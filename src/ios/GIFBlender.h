7822f4c9564f1233c0741a07d2e1871094d06804
Varsity Software <github@varsitysoftware.com>
2016-10-23 15:19:31 -0400

    Update GIFBlender.h

--- src/ios/GIFBlender.h
+++ src/ios/GIFBlender.h
@@ -12,6 +12,24 @@
 
 @property (copy)   NSString* callbackId;
 @property (nonatomic, retain) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) UIView* rootView; 
+@property (strong, nonatomic) NSURL* localURL;
+@property (strong, nonatomic) UIImage* imageOverlay; 
+
+@property(readwrite, nonatomic) int pixelDensity;
+
+@property(readwrite, nonatomic) float loadDelay;
+
+@property(readwrite, nonatomic) int gifWidth;
+@property(readwrite, nonatomic) int gifHeight;
+
+@property(readwrite, nonatomic) int quality;
+
+@property(readwrite, nonatomic) int numberOfFramesTotal;
+@property(readwrite, nonatomic) int numberOfFramesToUse; 
+
+@property (readwrite, nonatomic) CDVPluginResult* pluginResult; 
+@property (readwrite, nonatomic) NSMutableDictionary* jsonResults; 
 
 - (void)blend:(CDVInvokedUrlCommand *)command;
 
